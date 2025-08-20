45b72c4dd2d840f7e106b40e84e47dddee6a81e9
Ashmita Haldar <67264881+ashmitahaldar@users.noreply.github.com>
2025-08-20 15:13:56 +0800

    MayoBot.java: Add skeletal greeting functionality
    Chatbot only prints out a "hello" message along with a logo.
    
    No functionality is implemented, and the Duke project is not differentiated from the template.
    
    Implementing the basic skeletal functionality where the bot simple greets the user and exits is the first step to the breadth-first development approach for this project. Also, renaming it will allow for differentiation and adding personal touch.
    
    Let's add basic functionality to the project where the bot introduces its name, greets the user, and exits. Let's also rename Duke to MayoBot to add personalization.

---
+++ src/main/java/MayoBot.java
@@ -0,0 +1,30 @@
+public class MayoBot {
+    public static void main(String[] args) {
+        String name = "MayoBot";
+        printLogo();
+        printLine();
+        System.out.println("Hello, I'm " + name);
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printLine() {
+        String line = "--------------------------------------------------------------------------------------";
+        System.out.println(line);
+    }
+
+    private static void printLogo() {
+        String logo = ",---.    ,---.   ____       ____     __   ,-----.     _______       ,-----.  ,---------.\n"
+                + "|    \\  /    | .'  __ `.    \\   \\   /  /.'  .-,  '.  \\  ____  \\   .'  .-,  '.\\          \\\n"
+                + "|  ,  \\/  ,  |/   '  \\  \\    \\  _. /  '/ ,-.|  \\ _ \\ | |    \\ |  / ,-.|  \\ _ \\`--.  ,---'\n"
+                + "|  |\\_   /|  ||___|  /  |     _( )_ .';  \\  '_ /  | :| |____/ / ;  \\  '_ /  | :  |   \\\n"
+                + "|  _( )_/ |  |   _.-`   | ___(_ o _)' |  _`,/ \\ _/  ||   _ _ '. |  _`,/ \\ _/  |  :_ _:\n"
+                + "| (_ o _) |  |.'   _    ||   |(_,_)'  : (  '\\_/ \\   ;|  ( ' )  \\: (  '\\_/ \\   ;  (_I_)\n"
+                + "|  (_,_)  |  ||  _( )_  ||   `-'  /    \\ `\"/  \\  ) / | (_{;}_) | \\ `\"/  \\  ) /  (_(=)_)\n"
+                + "|  |      |  |\\ (_ o _) / \\      /      '. \\_/``\".'  |  (_,_)  /  '. \\_/``\".'    (_I_)\n"
+                + "'--'      '--' '.(_,_).'   `-..-'         '-----'    /_______.'     '-----'      '---'\n";
+        System.out.println(logo);
+    }
+}
