442c5d42a2ac2a7d59d4805916334ca40226f9d4
Ashmita Haldar <67264881+ashmitahaldar@users.noreply.github.com>
2025-08-21 11:21:21 +0800

    Task related classes: Add classes for Task and TaskList
    Tasks are managed in the Task private static class within the main MayoBot class.
    
    As the project expands, it would be preferable to manage Task and lists of Tasks separately in their own classes.
    
    Let's add new class files Task.java and TaskList.java to manage these data objects and add corresponding operations to help with managing the code and commands of the main MayoBot chatbot.
    
    Using separate classes is preferable to maintain OOP principles in the project.

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String task) {
+        tasks.add(new Task(task));
+    }
+
+    public void printTask(int index) {
+        System.out.println("\t" + tasks.get(index - 1));
+    }
+
+    public void printTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            System.out.println("\t" + (i + 1) + ". " + task);
+        }
+    }
+
+    public boolean markTaskAsDone(int index) {
+        if (index <= tasks.size()) {
+            tasks.get(index - 1).markAsDone();
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean markTaskAsNotDone(int index) {
+        if (index <= tasks.size()) {
+            tasks.get(index - 1).markAsNotDone();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
